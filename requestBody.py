66b8e31b1e4c3b9a74457b26258715e14c5ff9af
Sayli Patil <www.saylipatil1603@gmail.com>
2023-08-19 15:39:46 +0530

    19 aug 2023

--- requestBody.py
+++ requestBody.py
@@ -15,20 +15,58 @@
 #     return item
 
 
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+# app = FastAPI()
+
+# # items_dict = []
+
+
+# # @app.get("/items/")
+# # async def get_items():
+# #     return items_db
+
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     return item_dict
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 
-class Item(BaseModel):
+class Contact(BaseModel):
     name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+    full_name: str = None
+    phone: int
+    email: str = None
 
 
 app = FastAPI()
 
+contacts_db = []
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+
+@app.post("/contacts/")
+async def create_contact(contact: Contact):
+    contacts_db.append(contact)
+    return {"message": "Contact created successfully"}
+
+
+@app.get("/contacts/")
+async def get_contacts():
+    return contacts_db
