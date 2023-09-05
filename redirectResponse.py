0c05c74acfafb7bf8d03b0314448fdbebf9a10af
Sayli Patil <www.saylipatil1603@gmail.com>
2023-09-05 22:54:31 +0530

    5 sept 2023

--- redirectResponse.py
+++ redirectResponse.py
@@ -24,32 +24,79 @@ fetch url and redirect steps
 2) redirect the user to long url
 
 
-"""
+# # """
+import random
+import string
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+
 from pydantic import BaseModel
+import sqlite3
 
 app = FastAPI()
 
 
-url_mapping = {}
+# url_mapping = {}
+# Connect to the SQLite database
+conn = sqlite3.connect("url_mapping.db")
+cursor = conn.cursor()
+
+
+# Create a table to store the URL mapping
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS url_mapping (
+        short_url TEXT PRIMARY KEY,
+        long_url TEXT
+    )
+"""
+)
+
+conn.commit()
 
 
 class url(BaseModel):
     url: str
 
 
+def generate_random_string(length=5):
+    string = []
+    # characters to make random string
+    # characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    # use random.choice to select random characters and join them to create string
+    for i in range(length):
+        random_string = random.choice(characters)
+        string.append(random_string)
+    # print(string)
+    return string
+    #  Check if it already exists in the database
+    cursor.execute("SELECT COUNT(*) FROM url_mapping WHERE short_url = ?", (string,))
+    count = cursor.fetchone()[0]
+    if count == 0:
+        return short_url
+
+
 @app.post("/bit.ly")
 async def get_redirect(url: url):
     # generate short url
-    short_url = "abs"
+    short_url = generate_random_string(length=5)
     # store in database
-    return short_url
+    cursor.execute(
+        "INSERT INTO url_mapping (short_url, long_url) VALUES (?, ?)",
+        (short_url, url.url),
+    )
+    conn.commit()
+    return {"short_url": short_url}
 
 
 @app.get("/bit.ly/{urlID}")
-async def redirect_short_url(url: url):
-    # fetch long url using short url
-    long_url = url_mapping.get(short_url)
-    # redirect to long url
-    return {"redirect_url": long_url}
+async def redirect_short_url(urlID: str):
+    # Query database for the long URL
+    cursor.execute("SELECT long_url FROM url_mapping WHERE short_url = ?", (urlID,))
+    result = cursor.fetchone()
+    if result:
+        long_url = result[0]
+        return {"redirect_url": long_url}
+    else:
+        return {"error": "Short URL not found"}
